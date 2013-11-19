7f8a610a23679acdf126d9d09721b14eb13a3f61
unknown <aferreyr@AFERREYR-MOBL.amr.corp.intel.com>
2013-11-18 21:40:35 -0300

    another basic test for runtime

--- poc/unit-tests/test.js
+++ poc/unit-tests/test.js
@@ -6,3 +6,8 @@ test("number constant test", function() {
     var result = evalPuma("1");
     ok( result.success && 1 === result.value, "Passed!" );
 });
+
+test("string constant test", function() {
+    var result = evalPuma("\"Hello\"");
+    ok( result.success && "Hello" === result.value, "Passed!" );
+});
