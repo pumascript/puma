97b9e826248674f655a2863238d050778c8ce4ff
emravera <ravera.emanuel@gmail.com>
2014-04-09 20:01:41 -0300

    Added new benchmark  of checked class

---
+++ benchmarks/suites/testCheckedAttribute.js
@@ -0,0 +1,31 @@
+/*
+* Benchmark Number: 1
+* Description : attr.(Checked) vs Native .checked
+*/
+
+// Benchmark 1 
+var test1 = new Benchmark("Checked", "Jquery Attr Checked");
+
+test1.prepare = function() {    
+  $("body").append('<input type="checkbox" value="y" name="my-test-checkbox" id="my-test-checkbox" checked>');  
+};
+test1.test = function() {
+  $("#my-test-checkbox").attr("checked");
+};
+test1.clean = function() {
+  $("body").children().remove();
+};
+
+// Benchmark 2 
+var test2 = new Benchmark("Native checked", "Test Native .");
+test2.prepare = test1.prepare;
+
+test2.test = function() {
+  $("#my-test-checkbox")[0].checked;
+};
+test2.clean = test1.clean;
+
+//Creation of the test suite
+var testChecked = new Suite("Checked Attr");
+testChecked.addTest(test1);
+testChecked.addTest(test2);
