bd062cbeeded0e15ee32a0b8db76d6fcd5ef29a3
emravera <ravera.emanuel@gmail.com>
2014-04-28 21:21:28 -0300

    Added new infrastructure to benchmarks and first iteration for Tests Selectors

--- benchmarks/main.js
+++ benchmarks/main.js
@@ -1,24 +1,27 @@
 /*
 * Benchmark Engine Rules for Developers
 *   1-Define the tests Suites to run using the filename of the javascript 
-*   in the suites array in runAllEnabledTestSuites() method in the form {"name" : testSelectors_1}.
+*   in the suites array in runAllEnabledTestSuites() method in the form {"name" : testSelectors_1, "enabled": true|false }.
 *   2-The name of the suite inside the file must be the same as the name of the file.
 *   3-Include the JavaScript file in the benchmark.html head.
 */
 
 function runAllEnabledTestSuites(){  
-
+  
+  // Define the test suites using the names as in the files
   var suites = [ 
-                //{"name" : testSelectors_1}, 
-                //{"name" : testSelectors_2},
-                //{"name" : testSelectors_3},
-                //{"name" : testChecked},
-                //{"name" : testParseNumber},
-                  {"name" : testLoopFor}                  
+                {"name" : testSelectors_1, "enabled": true }, 
+                {"name" : testSelectors_2, "enabled": false },
+                {"name" : testSelectors_3, "enabled": false},
+                {"name" : testChecked, "enabled": false},
+                {"name" : testParseNumber, "enabled": false},
+                {"name" : testLoopFor, "enabled": false}                  
                ];
-
+  
+  //Run the enabled suites
   for(var i=0; i<suites.length; i++) {
-    //Run the enabled suites
-    suites[i].name.execute();
+    if(suites[i].enabled === true && suites[i].enabled !== undefined) {     
+      suites[i].name.execute();
+    }
   }
 };
