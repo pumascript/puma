68bb19c823fe83407e039b47d84bedec45aaf8b6
Albertina Durante <alberakd@gmail.com>
2015-04-10 20:22:59 -0300

    Moving libraries to require configuration and load them using requirejs. Codemirror is in progress.

--- editor/editor.js
+++ editor/editor.js
@@ -1,69 +1,89 @@
-var PumaEditor = function() {
-  this._javaScriptEditor = this.instantiateCodeMirrorEditor("javascript");
-  this._pumaScriptEditor = this.instantiateCodeMirrorEditor("puma"); 
-  this.registerEvents();
-};
+'use strict';
 
-PumaEditor.prototype.instantiateCodeMirrorEditor = function(section) {
-    return  CodeMirror(document.getElementById(section), {
-      mode: "text/javascript",
-      extraKeys: {"Ctrl-Space": "autocomplete", "Ctrl-J": "autocomplete"},
-      value: "",
-      lineNumbers: true,
-      height: 'auto',
-      tabSize:2,
-      foldGutter: true,
-      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
-    }); 
-};
+require.config({
+    paths: {
+        jquery: 'libs/jquery/jquery-1.9.1.min',
+        bootstrap: 'libs/bootstrap/js/bootstrap',
+        pumascript: '../src/pumascript'
+    },
+    shim: {
+        bootstrap: {
+            exports: 'bootstrap',
+            deps: ['jquery']
+        }
+    }
+});
 
-PumaEditor.prototype.editorValue = function(editor) {
-  return editor.getValue();
-};
+require([
+    'jquery',
+    'pumascript',
+    'bootstrap'
+], function ($, puma) {
 
-PumaEditor.prototype.setEditorValue = function(editor, value) {
-  return editor.setValue(value);
-};
+    function PumaEditor() {
+        this._javaScriptEditor = this.instantiateCodeMirrorEditor("javascript");
+        this._pumaScriptEditor = this.instantiateCodeMirrorEditor("puma");
+        this.registerEvents();
+    }
 
-PumaEditor.prototype.load = function(editor) {
-  this.setEditorValue(this._pumaScriptEditor, localStorage.getItem("puma"));
-};
+    PumaEditor.prototype.instantiateCodeMirrorEditor = function (section) {
+        return CodeMirror(document.getElementById(section), {
+            mode: "text/javascript",
+            extraKeys: {
+                "Ctrl-Space": "autocomplete",
+                "Ctrl-J": "autocomplete"
+            },
+            value: "",
+            lineNumbers: true,
+            height: 'auto',
+            tabSize: 2,
+            foldGutter: true,
+            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+        });
+    };
 
-PumaEditor.prototype.loadBackup = function(editor) {
-  this.setEditorValue(this._pumaScriptEditor, localStorage.getItem("puma-backup"));
-};
+    PumaEditor.prototype.editorValue = function (editor) {
+        return editor.getValue();
+    };
 
-PumaEditor.prototype.translate = function() {
-  var result = {};
-  var programStr = this.editorValue(this._pumaScriptEditor);
-  
-  //Save the last execution in local storage
-  if(programStr !== '') {
-    localStorage.setItem("puma-backup", localStorage.getItem("puma"));
-    localStorage.setItem("puma", programStr);    
-  }
-  
-  if(programStr !== undefined && programStr !== null) {
-      var that = this;
-      require(['pumascript'], function(puma){
-          result = puma.evalPuma(programStr);
-          that.setEditorValue(that._javaScriptEditor, result.output);
-          console.log(result);
-      });
-  }
-};
+    PumaEditor.prototype.setEditorValue = function (editor, value) {
+        return editor.setValue(value);
+    };
 
-PumaEditor.prototype.registerEvents = function(){
-  that = this;
-  $("#translatePuma").click(function() {
-    that.translate();
-  });
-  
-  $("#loadPuma").click(function() {
-    that.load();
-  });
-  
-  $("#loadBackupPuma").click(function() {
-    that.loadBackup();
-  });
-};
+    PumaEditor.prototype.load = function (editor) {
+        this.setEditorValue(this._pumaScriptEditor, localStorage.getItem("puma"));
+    };
+
+    PumaEditor.prototype.loadBackup = function (editor) {
+        this.setEditorValue(this._pumaScriptEditor, localStorage.getItem("puma-backup"));
+    };
+
+    PumaEditor.prototype.translate = function () {
+        var result = {};
+        var programStr = this.editorValue(this._pumaScriptEditor);
+
+        //Save the last execution in local storage
+        if (programStr !== '') {
+            localStorage.setItem("puma-backup", localStorage.getItem("puma"));
+            localStorage.setItem("puma", programStr);
+        }
+
+        if (programStr !== undefined && programStr !== null) {
+            result = puma.evalPuma(programStr);
+
+            this.setEditorValue(this._javaScriptEditor, result.output);
+
+            console.log(result);
+        }
+    };
+
+    PumaEditor.prototype.registerEvents = function () {
+        $("#translatePuma").click(this.translate.bind(this));
+
+        $("#loadPuma").click(this.load.bind(this));
+
+        $("#loadBackupPuma").click(this.loadBackup.bind(this));
+    };
+
+    var pumaEditorInstance = new PumaEditor();
+});
