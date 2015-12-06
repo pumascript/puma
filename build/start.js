8fb2c076d80961873771d3199a42c37ff06baf55
Albertina Durante <alberakd@gmail.com>
2015-12-06 03:29:24 -0300

    Fixing loading modules and libs for browser and node.

--- build/start.js
+++ build/start.js
@@ -1,26 +1,20 @@
 (function (root, factory) {
-    var esprimaLib = '../thirdparty/esprima/esprima.js',
-        escodegenLib;
+    var esprimaLib = '../thirdparty/esprima/esprima.js';
 
     if (typeof define === 'function' && define.amd) {
         // Browser support
-        escodegenLib = '../thirdparty/escodegen/escodegen.browser.js';
-
         deps = [
-            escodegenLib,
+            '../thirdparty/escodegen/escodegen.browser.js',
             esprimaLib
         ];
 
         // AMD. Register as an anonymous module.
         define(deps, factory);
     } else if (typeof exports === 'object') {
-        escodegenLib = '../node_modules/escodegen/escodegen.js';
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports,
-        // like Node.
+        // Node
         deps = [
-            escodegenLib,
-            esprimaLib
+            require('escodegen'),
+            require(esprimaLib)
         ];
 
         module.exports = factory.apply(null, deps);
