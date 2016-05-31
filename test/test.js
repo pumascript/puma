6ed6b1d0f9813e0f9123058c44593eb96caf470c
Afua Kwafo-Awua <afua.kw@gmail.com>
2016-05-31 01:49:42 -0300

    Standard 13 done

--- test/test.js
+++ test/test.js
@@ -477,7 +477,39 @@ define(['pumascript', 'esprima'], function (puma, esprima) {
         test("Creating Function Objects", function () {
         var result = puma.evalPuma("var F = function Add(x,y){return x+y}; F(2,3)");
         result.makeValue();
-        equal(result.value, 5, "Passed!");
+        equal(result.value,'5' , "Passed!");
     });
-
+    
+    test("Creating Function Objects 2", function () {
+        var result = puma.evalPuma("function add (x, y) {return x+y;}var sum = add(2,3);sum;");
+        result.makeValue();
+        equal(result.value,'5' , "Passed!");
+    });
+    
+    test("Creates a function and uses prototype method", function () {
+        var result = puma.evalPuma("function operations (x,y) {this.x = x; this.y = y;} Object.defineProperties(operations.prototype, { varX: {get: function () { return this.x; },set: function (value) {this.x = value;}},varY: {get: function () {return this.y;},set: function (value) {this.y = value;}}});operations.prototype.add = function () {return this.x+this.y;};operations.prototype.sub = function () {return this.x-this.y;};operations.prototype.div = function () {if(this.y !== 0) {return this.x/this.y;}};operations.prototype.mul = function () {return this.x*this.y;};var op = new operations(2,3);op.mul();");
+        result.makeValue();
+        equal(result.value,'6' , "Passed!");
+    });
+    
+    test("Creates a function and gets its internal value with a getter function", function () {
+        var result = puma.evalPuma("function operations (x,y) {this.x = x; this.y = y;} Object.defineProperties(operations.prototype, { varX: {get: function () { return this.x; },set: function (value) {this.x = value;}},varY: {get: function () {return this.y;},set: function (value) {this.y = value;}}});operations.prototype.add = function () {return this.x+this.y;};operations.prototype.sub = function () {return this.x-this.y;};operations.prototype.div = function () {if(this.y !== 0) {return this.x/this.y;}};operations.prototype.mul = function () {return this.x*this.y;};var op = new operations(2,3);op.varX;");
+        result.makeValue();
+        equal(result.value,'2' , "Passed!");
+    });
+    
+    test("Creates a function and calls its internal method", function () {
+        var result = puma.evalPuma("function Square (side) {this.side = side;this.area = function () {return this.side * this.side;}}var s1 = new Square(2);s1.area();");
+        result.makeValue();
+        equal(result.value,'4' , "Passed!");
+    });
+    
+    test("Function Object creation", function () {
+        var result = puma.evalPuma("var test = new Object();");
+        result.makeValue();
+        equal(result.value,'undefined' , "Passed!");
+    });
+    
+    //ECMA 14
+    
 });
