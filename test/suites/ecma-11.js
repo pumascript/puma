59e8022395e583ddcbacfe79ecb2502da515ca2e
leolujan <leonel.anid@gmail.com>
2016-08-02 16:17:47 -0300

    added test

--- test/suites/ecma-11.js
+++ test/suites/ecma-11.js
@@ -14,15 +14,6 @@ define(['pumascript', 'esprima'], function(puma, esprima) {
  *                      Expressions
  */
 
-    /*
-
-     test("",function () {
-     var result = puma.evalPuma("");
-     ok(result.success,"Passed!");
-     equal(result.value(),,"Passed!");
-     });
-
-    */
     module("11.1 Primary Expressions");
 
     test("The this Keyword",function () {
@@ -48,12 +39,165 @@ define(['pumascript', 'esprima'], function(puma, esprima) {
     });
 
     module("11.2 Left-Hand-Side Expressions");
+
+    test("Property Accessors",function () {
+        var result = puma.evalPuma("var object = {}; object['property'] = 'value'; object['property'];");
+        result.makeValue();
+        equal(result.value(),'value',"Passed!");
+    });
+
+    test("The new operator",function () {
+        var result = puma.evalPuma("function Car(make, model, year) {this.make = make; this.model = model; this.year = year;} var mycar = new Car('Eagle', 'Talon TSi', 1993);");
+        ok(result.success,"Passed!");
+    });
+
+    test("Function Calls",function () {
+        var result = puma.evalPuma("var square = function(number) { return number * number }; square(4)");
+        result.makeValue();
+        equal(result.value, 16,"Passed!");
+    });
+
+
+    /*11.2.4 Argument Lists
+
+    11.2.5 Function Expressions
+    */
+
     module("11.3 Postfix Expressions.");
+
+    test("Increment",function () {
+        var result = puma.evalPuma("var x = 0; x++; ");
+        result.makeValue();
+        equal(result.value(),1,"Passed!");
+    });
+
+    test("Decrement",function () {
+        var result = puma.evalPuma("var x = 1; x--; ");
+        result.makeValue();
+        equal(result.value(),0,"Passed!");
+    });
+
     module("11.4 Unary Operators");
+
+    test("Delete Operator",function () {
+        var result = puma.evalPuma("var x = {}; x.property = 'property'; delete x.property; x.property;");
+        result.makeValue();
+        equal(result.value(),null,"Passed!");
+    });
+
+    test("Void Operator",function () {
+        var result = puma.evalPuma("void 0");
+        equal(result.value(),undefined,"Passed!");
+    });
+
+    test("TypeOf Operator",function () {
+        var result = puma.evalPuma("typeof true;");
+        equal(result.value(),boolean,"Passed!");
+    });
+
+    test("Prefix Increment Operator",function () {
+        var result = puma.evalPuma("var x=0; ++x;");
+        result.makeValue();
+        equal(result.value(),1,"Passed!");
+    });
+
+    test("Prefix Decrement Operator",function () {
+        var result = puma.evalPuma("var x=1; --x;");
+        result.makeValue();
+        equal(result.value(),0,"Passed!");
+    });
+
+    test("Unary + Operator",function () {
+        var result = puma.evalPuma("var x=-10; +x;");
+        result.makeValue();
+        equal(result.value(),-10,"Passed!");
+    });
+
+    test("Unary - Operator",function () {
+        var result = puma.evalPuma("var x=-10; -x;");
+        result.makeValue();
+        equal(result.value(),10,"Passed!");
+    });
+
+    test("Bitwise NOT Operator ( ~ )",function () {
+        var result = puma.evalPuma("var x= 10; ~x;");
+        result.makeValue();
+        equal(result.value(),-11,"Passed!");
+    });
+
+    test("Logical NOT Operator ( ! )",function () {
+        var result = puma.evalPuma("!false;");
+        equal(result.value(),true,"Passed!");
+    });
+
     module("11.5 Multiplicative Operators and 11.6 Additive Operators");
+
+     test("Multiplicative and Additive Operators",function () {
+         var result = puma.evalPuma("2 * 3 / 4 + 5 - 6 % 2;");
+         result.makeValue();
+         equal(result.value(),6.5,"Passed!");
+     });
+
     module("11.7 Bitwise Shift Operators");
+
+     test("The Left Shift Operator ( << )",function () {
+        var result = puma.evalPuma("8 << 2;");
+        equal(result.value(),32,"Passed!");
+    });
+
+    test("The Signed Right Shift Operator ( >> )",function () {
+        var result = puma.evalPuma("9 >> 2;");
+        equal(result.value(),2,"Passed!");
+    });
+
+    test("The Unsigned Right Shift Operator ( >>> )",function () {
+        var result = puma.evalPuma("19>>>2");
+        equal(result.value(),4,"Passed!");
+    });
+
     module("11.8 Relational Operators");
+
+     test("< Operator",function () {
+        var result = puma.evalPuma("0 < 1;");
+        ok(result.value,"Passed!");
+    });
+
+    test("> Operator",function () {
+        var result = puma.evalPuma("0 > 1;");
+        ok(!result.value,"Passed!");
+    });
+
+    test("<= Operator",function () {
+        var result = puma.evalPuma(" 1 <= 1 && 0 <= 1;");
+        ok(result.value,"Passed!");
+    });
+
+    test(">= Operator",function () {
+        var result = puma.evalPuma("1 >= 1 && 1 >= 0;");
+        ok(result.value,"Passed!");
+    });
+
+    test("instanceof Operator",function () {
+        var result = puma.evalPuma("var x={}; x instanceof Object;");
+        result.makeValue();
+        equal(result.value(),true,"Passed!");
+    });
+
+    test("In Operator",function () {
+        var result = puma.evalPuma("1 in [0,1];");
+        ok(result.value,"Passed!");
+    });
+
     module("11.9 Equality Operators");
+    /*
+
+     test("",function () {
+     var result = puma.evalPuma("");
+     ok(result.success,"Passed!");
+     equal(result.value(),,"Passed!");
+     });
+
+     */
     module("11.10 Binary Bitwise Operators");
     module("11.11 Binary Logical Operators");
     module("11.12 Conditional Operators");
