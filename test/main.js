8688009198f9cfa2c8d86fa810ce49e39df9dac9
Pignatar0md <pignataromd@gmail.com>
2016-09-20 20:16:24 -0300

    test section 10 ecmascript

--- test/main.js
+++ test/main.js
@@ -20,13 +20,13 @@ var suites = [
         './suites/base-tests.js',
         './suites/runtime-tests.js',
         './suites/integration-tests.js',
-        './suites/Tests-7-ECMA.js',
-        './suites/Tests-8-ECMA.js',
-        './suites/test-9-ecma.js',
+        /*'./suites/Tests-7-ECMA.js',
+        './suites/Tests-8-ECMA.js',*/
+        //'./suites/test-9-ecma.js',
         './suites/test-10-ecma.js',
-        './suites/test-12-ecma.js',
+        /*'./suites/test-12-ecma.js',
         './suites/test-13-ecma.js',
-        './suites/Tests-15-ECMA.js'
+        './suites/Tests-15-ECMA.js'*/
 ];
 
 require(suites, function () {
