ae7c9b5e12922464556c007df772e3fa7f7cf417
Pignatar0md <pignataromd@gmail.com>
2016-09-27 20:03:43 -0300

    fix things commented by mates

--- test/main.js
+++ test/main.js
@@ -6,10 +6,10 @@
  */
 
 require.config({
-    paths: {
-        'pumascript': '../src/pumascript',
-        'esprima': '../thirdparty/esprima/esprima'
-    }
+  paths: {
+    'pumascript': '../src/pumascript',
+    'esprima': '../thirdparty/esprima/esprima'
+  }
 });
 
 
@@ -17,20 +17,20 @@ require.config({
  * @enum containing the list of test suites to execute
  */
 var suites = [
-        './suites/base-tests.js',
-        './suites/runtime-tests.js',
-        './suites/integration-tests.js',
-        './suites/Tests-7-ECMA.js',
-        './suites/Tests-8-ECMA.js',
-        './suites/test-9-ecma.js',
-			  './suites/test-10-ecma.js',
-        './suites/Tests-11-ECMA.js',
-        './suites/test-12-ecma.js',
-        './suites/test-13-ecma.js',
-        './suites/Tests-15-ECMA.js'
+  './suites/base-tests.js',
+  './suites/runtime-tests.js',
+  './suites/integration-tests.js',
+  './suites/Tests-7-ECMA.js',
+  './suites/Tests-8-ECMA.js',
+  './suites/test-9-ecma.js',
+	'./suites/test-10-ecma.js',
+  './suites/Tests-11-ECMA.js',
+  './suites/test-12-ecma.js',
+  './suites/test-13-ecma.js',
+  './suites/Tests-15-ECMA.js'
 ];
 
 require(suites, function () {
-    // Required to make it work under phantomjs environment
-    QUnit.start();
+  // Required to make it work under phantomjs environment
+  QUnit.start();
 });
