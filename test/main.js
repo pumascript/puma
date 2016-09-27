5920965bcc413089408f065eb46d54b3a71bd0d9
Pignatar0md <pignataromd@gmail.com>
2016-09-27 20:18:07 -0300

    fix things commented by mates

--- test/main.js
+++ test/main.js
@@ -23,7 +23,7 @@ var suites = [
   './suites/Tests-7-ECMA.js',
   './suites/Tests-8-ECMA.js',
   './suites/test-9-ecma.js',
-	'./suites/test-10-ecma.js',
+  './suites/test-10-ecma.js',
   './suites/Tests-11-ECMA.js',
   './suites/test-12-ecma.js',
   './suites/test-13-ecma.js',
