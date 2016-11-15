c8466cca139423943cbaabeb7e6715ea651cf56e
Albertina Durante <albertina.durante@intel.com>
2016-11-15 16:12:10 -0300

    Adding bin folder for cli utility
    - Move puma-cli to bin folder
    - Update indentation of package.json to 4 spaces

---
+++ bin/pumascript.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// Copyright (c) 2013 - present UTN-LIS
+
+var program = require('commander'),
+    puma = require('../dist/pumascript.js'),
+    fs = require('fs');
+
+program
+    .usage('<output> <filePaths ...>')
+    .option('-o, --output <output>', 'Name and path of the output file')
+    .parse(process.argv);
+
+if (!program.args.length) {
+    program.help();
+} else {
+    if (program.output) {
+        var pumaFile = '';
+
+        program.args.forEach(function (file) {
+            console.info('Processing: ', file, ' file.');
+            pumaFile += fs.readFileSync(file, 'utf8');
+        });
+
+        var parsedFile = puma.evalPuma(pumaFile);
+
+        fs.writeFile(program.output, parsedFile.output, 'utf8', function (err) {
+            if (err) throw err;
+            console.log('File Saved!');
+        });
+    } else {
+        console.log('Specify a destination output');
+    }
+}
