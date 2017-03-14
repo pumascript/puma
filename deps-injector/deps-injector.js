ea0fbe3d53733b63ff96db65d42289b4dcaa757c
Emanuel M Ravera <ravera.emanuel@gmail.com>
2017-03-13 21:29:40 -0300

    Added integrity check

--- deps-injector/deps-injector.js
+++ deps-injector/deps-injector.js
@@ -14,8 +14,10 @@ require(['pumascript'], function(puma){
         this.deps = dependencies || [];
         this.installDeps();
     };
+
+    PumaInjector.FORCE_INJECT = true;
     
-    PumaInjector.prototype.getExternalScript = function(url) {
+    PumaInjector.prototype.getExternalScript = function(dependency) {
         console.log('Getting the Lib from web');
         var xhttp = new XMLHttpRequest();
         var that = this;
@@ -24,22 +26,25 @@ require(['pumascript'], function(puma){
                 // Action to be performed when the document is read;
                 try {
                     var result = that.puma.evalPuma(this.responseText);
-                    if(result.success) that.createScriptNode(url);
+                    if(result.success) that.createScriptNode(dependency);
                 }
                 catch (e) {
                     console.log('Error in puma interpretation');
                     console.error(e);
+                    if(PumaInjector.FORCE_INJECT) that.createScriptNode(dependency);
                 }
             }
         };
-        xhttp.open("GET", url, true);
+        xhttp.open("GET", dependency.url, true);
         xhttp.send();
     };
 
 
-    PumaInjector.prototype.createScriptNode = function(url){
+    PumaInjector.prototype.createScriptNode = function(dependency){
         var script = document.createElement('script');
-        script.src = url;
+        script.src = dependency.url;
+        script.setAttribute("crossorigin", "anonymous");
+        script.integrity = dependency.checksum;
         document.getElementsByTagName('head')[0].appendChild(script);
     };
 
@@ -52,5 +57,6 @@ require(['pumascript'], function(puma){
         }
     };
     
-    return new PumaInjector(["https://code.jquery.com/jquery-2.2.4.js"]);
+    return new PumaInjector([{url: "https://code.jquery.com/jquery-2.2.4.js",
+                             checksum: "sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="}]);
 });
