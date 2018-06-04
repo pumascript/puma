15c5c4d3c9bdb25592b4cbdf529aa845f86b5a44
Paolo Emilio Mattio <pemattio@gmail.com>
2018-06-04 17:26:01 -0300

    Fixed indentation constants and variables & map and set

--- test/suites/ecma6-tests/ecma6-map-set-and-weakmap-weakset-tests.js
+++ test/suites/ecma6-tests/ecma6-map-set-and-weakmap-weakset-tests.js
@@ -10,42 +10,42 @@ var test = QUnit.test;
 var skip = QUnit.skip;
 
 define(['pumascript'], function (puma) {
-    
+
     QUnit.module("Ecma6-Map-Set-And-WeakMap-WeakSet-Tests");
-    
+
     skip("Map get method", function(assert) {
         var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.get(s) === 34");
         assert.equal(result.value, true);
     });
-    
+
     skip("Map has method", function(assert) {
         var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.has(s) === true");
         assert.equal(result.value, true);
     });
-    
+
     skip("Map stores unique keys, check of size", function(assert) {
         var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.set('hello', 34); m.size === 2");
         assert.equal(result.value, true);
     });
-    
+
     skip("Map delete method", function(assert) {
         var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.delete('hello'); m.size === 1");
         assert.equal(result.value, true);
     });
-    
+
     skip("Map clear method", function(assert) {
         var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.clear(); m.size === 0");
         assert.equal(result.value, true);
     });
-    
+
     skip("Set has method", function(assert) {
         var result = puma.evalPuma("let s = new Set(); s.add('hello'); s.has('hello') === true;");
         assert.equal(result.value, true);
     });
-    
+
     skip("Set stores unique values, check of size", function(assert) {
         var result = puma.evalPuma("let s = new Set(); s.add('hello').add('goodbye').add('hello'); s.size === 2;");
         assert.equal(result.value, true);
     });
-    
+
 });
