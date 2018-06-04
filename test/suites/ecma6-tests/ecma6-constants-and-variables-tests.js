eca7531b6f727cdba1eed6123d4faefcc911cbc6
Paolo Emilio Mattio <pemattio@gmail.com>
2018-06-04 17:51:25 -0300

    Added result.success to ECMA 6 first tests

--- test/suites/ecma6-tests/ecma6-constants-and-variables-tests.js
+++ test/suites/ecma6-tests/ecma6-constants-and-variables-tests.js
@@ -3,8 +3,8 @@
 /* eslint quotes: 0, no-unused-vars: 0 */
 
 /**
-    *          PUMASCRIPT ECMA 6 CONSTANTS AND VARIABLES SUITE
-    * @file: Ecma6 Constants and variables expressions test suite for the language
+*          PUMASCRIPT ECMA 6 CONSTANTS AND VARIABLES SUITE
+* @file: Ecma6 Constants and variables expressions test suite for the language
 */
 var test = QUnit.test;
 var skip = QUnit.skip;
@@ -14,48 +14,56 @@ define(['pumascript'], function (puma) {
     QUnit.module("Ecma6-Constants-And-Variables-Tests");
 
     skip("Constant", function(assert) {
-        var result = puma.evalPuma("const PI = 3.141593; PI > 3.0;");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("const PI = 3.141593; PI;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 3.141593);
     });
 
     skip("Constant principal scope", function(assert) {
-        var result = puma.evalPuma("const PI = 3.141593; if(true){const PI = 3.14;} PI === 3.141593;");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("const PI = 3.141593; if(true){const PI = 3.14;} PI;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 3.141593);
     });
 
     skip("Constant block scope", function(assert) {
-        var result = puma.evalPuma("const PI = 3.141593; if(true){const PI = 3.14; PI === 3.14}");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("const PI = 3.141593; if(true){const PI = 3.14; PI}");
+        result.makeValue();
+        assert.ok(result.success && result.value === 3.14);
     });
 
     skip("Constant principal scope not defined with block scope constant", function(assert) {
-        var result = puma.evalPuma("if(true){const PI = 3.141593;} typeof(PI) === 'undefined';");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("if(true){const PI = 3.141593;} typeof(PI);");
+        result.makeValue();
+        assert.ok(result.success && result.value === 'undefined');
     });
 
     skip("Change of a property from a constant as an object", function(assert) {
-        var result = puma.evalPuma("const p = {first: 'john', last: 'smith'}; p.first = 'bob'; p.first === 'bob';");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("const p = {first: 'john', last: 'smith'}; p.first = 'bob'; p.first;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 'bob');
     });
 
     skip("Variable let", function(assert) {
-        var result = puma.evalPuma("let a = 2; a === 2");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let a = 2; a;");
+        assert.ok(result.success && result.value === 2);
     });
 
     skip("Variable let principal scope", function(assert) {
-        var result = puma.evalPuma("let a = 2; if(true){let a = 3;} a === 2;");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let a = 2; if(true){let a = 3;} a;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 2);
     });
 
     skip("Variable let block scope", function(assert) {
-        var result = puma.evalPuma("let a = 2; if(true){let a = 3; a === 3}");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let a = 2; if(true){let a = 3; a;}");
+        result.makeValue();
+        assert.ok(result.success && result.value === 3);
     });
 
     skip("Variable let principal scope not defined with block scope constant", function(assert) {
-        var result = puma.evalPuma("if(true){let a = 3;} typeof(a) === 'undefined';");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("if(true){let a = 3;} typeof(a);");
+        result.makeValue();
+        assert.ok(result.success && result.value === 'undefined');
     });
 
 });
--- test/suites/ecma6-tests/ecma6-map-set-and-weakmap-weakset-tests.js
+++ test/suites/ecma6-tests/ecma6-map-set-and-weakmap-weakset-tests.js
@@ -3,8 +3,8 @@
 /* eslint quotes: 0, no-unused-vars: 0 */
 
 /**
-    *          PUMASCRIPT ECMA 6 MAP-SET AND WEAKMAP-WEAKSET SUITE
-    * @file: Ecma6 Map-Set and WeakMap-WeakSet expressions test suite for the language
+*          PUMASCRIPT ECMA 6 MAP-SET AND WEAKMAP-WEAKSET SUITE
+* @file: Ecma6 Map-Set and WeakMap-WeakSet expressions test suite for the language
 */
 var test = QUnit.test;
 var skip = QUnit.skip;
@@ -14,38 +14,46 @@ define(['pumascript'], function (puma) {
     QUnit.module("Ecma6-Map-Set-And-WeakMap-WeakSet-Tests");
 
     skip("Map get method", function(assert) {
-        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.get(s) === 34");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.get(s);");
+        result.makeValue();
+        assert.ok(result.success && result.value === 34);
     });
 
     skip("Map has method", function(assert) {
-        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.has(s) === true");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.has(s);");
+        result.makeValue();
+        assert.ok(result.success && result.value === true);
     });
 
     skip("Map stores unique keys, check of size", function(assert) {
-        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.set('hello', 34); m.size === 2");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.set('hello', 34); m.size;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 2);
     });
 
     skip("Map delete method", function(assert) {
-        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.delete('hello'); m.size === 1");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.delete('hello'); m.size;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 1);
     });
 
     skip("Map clear method", function(assert) {
-        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.clear(); m.size === 0");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let m = new Map(); let s = Symbol(); m.set('hello', 42); m.set(s, 34); m.clear(); m.size;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 0);
     });
 
     skip("Set has method", function(assert) {
-        var result = puma.evalPuma("let s = new Set(); s.add('hello'); s.has('hello') === true;");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let s = new Set(); s.add('hello'); s.has('hello');");
+        result.makeValue();
+        assert.ok(result.success && result.value === true);
     });
 
     skip("Set stores unique values, check of size", function(assert) {
-        var result = puma.evalPuma("let s = new Set(); s.add('hello').add('goodbye').add('hello'); s.size === 2;");
-        assert.equal(result.value, true);
+        var result = puma.evalPuma("let s = new Set(); s.add('hello').add('goodbye').add('hello'); s.size;");
+        result.makeValue();
+        assert.ok(result.success && result.value === 2);
     });
 
+    //Missing WeakMap and WeakSet tests
 });
